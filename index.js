1146b64b9abaee063a7bb0cb7a4c5bf540cfdb94
FULLBL00M <asawyer@junglelasers.com>
2024-04-29 01:12:05 -0400

    This is a test

--- index.js
+++ index.js
@@ -25,16 +25,16 @@ function getFooterHeight() {
 // window.addEventListener('load', setMainHeight);
 // window.addEventListener('resize', setMainHeight);
 document.addEventListener('DOMContentLoaded', function () {
-    console.log("Todo List");
-    console.log("[ ] Make a newcomponent script to match the new page script");
-    console.log("[ ] Delete the old pages and recreate them after updating the page template");
-    console.log("[ ] Make a script that will automatically detect all pages and sub pages and create nav links in the header");
-    console.log("[ ] Make a hero component.");
-    console.log("[ ] Make the medium layout that they use for articles.");
-    console.log("[ ] Make a cheatsheet section of the site.");
-    console.log("[ ] Make the build script watch for changes to files and run automatically.");
-    console.log("[ ] Make a cheatsheet for Pug");
-    console.log("[ ] Make a cheatsheet for console log in js");
-    console.log("[ ] Make some layouts using CSS Grid");
-    console.log("[ ] Get the component scss out of the main file and let it be brought in the right way.");
+    // console.log("Todo List");
+    // console.log("[ ] Make a newcomponent script to match the new page script");
+    // console.log("[ ] Delete the old pages and recreate them after updating the page template");
+    // console.log("[ ] Make a script that will automatically detect all pages and sub pages and create nav links in the header");
+    // console.log("[ ] Make a hero component.");
+    // console.log("[ ] Make the medium layout that they use for articles.");
+    // console.log("[ ] Make a cheatsheet section of the site.");
+    // console.log("[ ] Make the build script watch for changes to files and run automatically.");
+    // console.log("[ ] Make a cheatsheet for Pug");
+    // console.log("[ ] Make a cheatsheet for console log in js");
+    // console.log("[ ] Make some layouts using CSS Grid");
+    // console.log("[ ] Get the component scss out of the main file and let it be brought in the right way.");
 });
